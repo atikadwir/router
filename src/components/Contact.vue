bd3e1b4766effce50417e9e23a3489342b83c9f1
atikadwi <atika_dwi_27rpl@student.smktelkom-mlg.sch.id>
2020-12-10 12:33:01 +0700

    first commit

---
+++ src/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>Contact</h1>
+        <h2>089681896848</h2>
+        <h2>Atika Dwi Rahmalia</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Contact"
+}
+</script>
